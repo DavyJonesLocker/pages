c824d1043bb581c68148a8ff742a0906702c3a42
Brian Cardarella <bcardarella@gmail.com>
2012-08-12 20:57:14 -0400

    Setup for TravisCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
-# Configure Rails Envinronment
+require 'rubygems'
+begin
+  require 'debugger'
+rescue LoadError
+end
+require 'bundler/setup'
+
 ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'capybara/rspec'
-
 require 'rspec/rails'
-if RUBY_VERSION > '1.9' && RUBY_ENGINE == 'ruby'
-  require 'debugger'
-end
 
 ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
